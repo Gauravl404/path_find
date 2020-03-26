764e11d9e46f7ee399859e364fe6efd52a6fd509
Souravs437 <skgen123@gmail.com>
2020-03-27 02:47:37 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Path_findevv from './path_findev/Path_findevv';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Path_findevv></Path_findevv>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/path_findev/Path_findevv.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+export default class path_find extends Component {
+    render() {
+        return (
+            <div>
+            <h1>hello world </h1>
+            </div>
+        );
+    }
+}
