5f011a02b3508f35b9f9f2f1733d1aab026af62e
Souravs437 <skgen123@gmail.com>
2020-04-05 14:30:32 +0530

    new

--- src/path_findev/Path_findevv.jsx
+++ src/path_findev/Path_findevv.jsx
@@ -1,12 +1,75 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Node from '../NODE/Node'
+import '../NODE/Node.css'
 
-export default class path_find extends Component {
+const st_r = 10;
+const st_c = 10;
+const des_r = 10;
+const des_c = 40;
+
+
+const make_grid = () => {
+    const grid = [];
+
+    for(let row = 0 ; row < 20 ; row++) {
+        const inrow = [];
+        for(let col = 0 ; col < 50 ; col++) {
+            inrow.push(createNode(row, col));
+        }
+        grid.push(inrow);
+    }
+    return grid;
+}
+
+const createNode = (row , col) => {
+    return {
+        row , col
+    };
+}
+
+
+const fun = () => {
+
+}
+
+export default class path_findevv extends Component {
+    constructor() {
+      super();
+      this.state = {
+        grid: [],
+        mouseIsPressed: false,
+      };
+    }
+
+    initializeComponent() {
+        const grid = make_grid();
+        this.setState({grid});
+    }
+    
     render() {
-        return (
-            <div>
-            <h1>hello world </h1>
+        const {grid, mouseIsPressed} = this.state;
+        
+        return(
+            <>
+            <button type ="button" onClick= {() => this.initializeComponent()}> grid</button>
+            <div className = "grid">
+                {grid.map((row , rowidx) => {
+                    return (
+                        <div key = {rowidx}>
+                            {row.map((node ,nodeidx) => {
+                                const   {row, col } = node;
+                                return <Node>
+                                    row = {row}
+                                    col = {col}
+                                </Node>
+                            })}
+                        </div>
+                    );
+                })}
             </div>
+            </>
         );
     }
-}
+};
+
